7bafaa465d0647ff424bad6cb25f5f6b5c80c084
Luis Parente <lgsp25hotmail.com>
2024-11-07 18:34:36 -0300

    User insert with Post

--- src/main/java/com/nelioalves/workshopmongo/services/UserService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.workshopmongo.domain.User;
+import com.nelioalves.workshopmongo.dto.UserDTO;
 import com.nelioalves.workshopmongo.repositories.UserRepository;
 import com.nelioalves.workshopmongo.services.exceptions.ObjectNotFoundException;
 
@@ -15,13 +16,21 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> user = repository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+
+	public User insert(User user) {
+		return repository.insert(user);
+	}
+
+	public User fromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}
 }
